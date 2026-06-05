9d3662ab6db5492ed5bf4939391c05b84f667ce9
prajwal-56 <prajwalpraveen2006@gmail.com>
2026-06-06 02:43:40 +0530

    more update and tweaks

---
+++ footer.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+  var footer = document.querySelector('footer');
+
+  if (footer) {
+
+    footer.innerHTML =
+      '<div class="footer-links">' +
+      '<a href="https://github.com/prajwal-56" target="_blank" rel="noopener">github</a>' +
+      '<a href="https://www.linkedin.com/in/prajwal-praveen/" target="_blank" rel="noopener">linkedin</a>' +
+      '<a href="https://www.instagram.com/prajwalp.__" target="_blank" rel="noopener">instagram</a>' +
+      '<a href="#" target="_blank" rel="noopener">x</a>' +
+      '</div>';
+
+  }
+
+});
